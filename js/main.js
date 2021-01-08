87f9e0a71c627d6ce6f7cd049a1278af99a19406
Alejo29 <diegoelandro-29@hotmail.com>
2021-01-08 10:53:18 -0500

    Cumple Wilo subida de archivos

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+    $('.cuenta-regresiva').countdown('2021/02/01 00:00:00', function(event){
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+      });
