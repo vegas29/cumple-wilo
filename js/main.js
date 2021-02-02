cc885edbbdea3b607e5fcc35a0fa158ded5d057f
Alejandro Perdomo <alejousco0708@gmail.com>
2021-02-01 23:49:15 -0500

    Actualizacion 2022 main.js
    Actualizacion al año 2022

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-
-
-    $('.cuenta-regresiva').countdown('2021/02/01 00:00:00', function(event){
-        $('#dias').html(event.strftime('%D'));
-        $('#horas').html(event.strftime('%H'));
-        $('#minutos').html(event.strftime('%M'));
-        $('#segundos').html(event.strftime('%S'));
-      });
+$('.cuenta-regresiva').countdown('2022/02/01 00:00:00', function(event) {
+    $('#dias').html(event.strftime('%D'));
+    $('#horas').html(event.strftime('%H'));
+    $('#minutos').html(event.strftime('%M'));
+    $('#segundos').html(event.strftime('%S'));
+});
